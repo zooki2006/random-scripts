e03d41484ee96e22ddbb234fe5de1b4271754c43
unknown <fleshshell@cock.li>
2022-05-07 12:44:30 +0100

    make binds.sh work with dash

--- binds.sh
+++ binds.sh
@@ -2,5 +2,5 @@
 
 grep XK_ $HOME/src/dwm/config.h | grep MODKEY | awk {'print $2 " " $3 " --- " $4 " " $7'} | grep -v { | sed 's/,//g' | sed 's/XK_//g' | sed 's/&//g' | sed 's/|ShiftMask/ Shift +/g' | sed 's/MODKEY/MOD +/g' | sed 's/|ControlMask/ Ctrl +/g' | sed 's/}//g' | column
 
+read test
 
-read
