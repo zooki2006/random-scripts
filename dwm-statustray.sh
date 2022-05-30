ee7ab42bb127e14c1b02713c1851dca7e682985f
zooki <fleshshell@cock.li>
2022-05-30 20:51:22 +0100

    clean up

--- dwm-statustray.sh
+++ dwm-statustray.sh
@@ -2,21 +2,27 @@
 dwmstat="on"
 while [ "$dwmstat" = on ]; do
 	tray=""
-	while read trayitems
-	do
-	       	program=$(echo $trayitems | awk '{print $1}')	
-	       	trayname=$(echo $trayitems | awk '{print $2}')
-		if pgrep -x "$program" > /dev/null
-		then
-			temptray="[$trayname]"
-			tray="$tray $temptray"
-		fi
-
-	done < ~/tray.conf
-	xsetroot -name "$tray $(date)"
-
-	sleep 0.9 
-	if ! pgrep -x "dwm" > /dev/null && ! pgrep -x "dwm-stat" > /dev/null
+#	while read trayitems
+#	do
+#	       	program=$(echo $trayitems | awk '{print $1}')	
+#	       	trayname=$(echo $trayitems | awk '{print $2}')
+#		if pgrep -x "$program" > /dev/null
+#		then
+#			temptray="[$trayname]"
+#			tray="$tray $temptray"
+#		fi
+#
+#	done < ~/tray.conf
+	tray=$(/opt/Ibin/scripts/zuki-tray.sh)
+	total=$(cat ~/.scratchpad/dynamic/dynamicnum)
+	if [ "$total" = 0 ]
+	then
+		xsetroot -name "$tray $(date)"
+	else
+		xsetroot -name "[$total] $tray $(date)"
+	fi
+	sleep 4.9 
+	if ! pgrep -x "dwm" > /dev/null && ! pgrep -x "wm-stat" > /dev/null
 	then
 		dwmstat="off"
 	fi
