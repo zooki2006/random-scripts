a06f7fb6e7ed43c2db938da8753f3a2faf737fa5
zooki <fleshshell@cock.li>
2022-05-30 20:50:59 +0100

    iegbfjdjfla

--- manlock.sh
+++ manlock.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 dimscreen=10
-killall xidlehook
-if [ ! $dimscreen = 0 ]
-then
-	sleep $dimscreen && xrandr --output "$PRIMARY_DISPLAY" --brightness .1 &
-fi
-slock
+pkill xidlehook
+xrandr --output "$PRIMARY_DISPLAY" --brightness 0
+slock 
 xrandr --output "$PRIMARY_DISPLAY" --brightness 1
-lock.sh  
+#lock.sh  
