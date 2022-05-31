486a322954ba1add38d9d6cfce2b44f154dbf6e8
zooki <fleshshell@cock.li>
2022-05-31 10:41:12 +0100

    add xpropalt to be used if user doesn't have wmctrl

--- Scratchpad2.sh
+++ Scratchpad2.sh
@@ -4,7 +4,18 @@ arg2="$2"
 arg3="$3"
 class1="$arg1"
 class2="$arg2"
+xpropalt () {
+winid=$(xprop -root _NET_CLIENT_LIST | sed 's!,!\n!g' | sed 's!#!\n!' | grep -v "_NET")
+wincid=$(echo "$winid" | wc -l)
+#classid=$(xprop -id 0xe0002d WM_CLASS | sed 's!=!\n!' | grep -v "WM_CLASS" | sed 's!"!!g' | sed 's#,##')
+for i in `seq $wincid`
+do
+	classid=$(xprop -id $(echo "$winid" | awk "NR==$i") WM_CLASS | sed 's!,!!g' | sed 's!=!\n!' | grep -v "WM_CLASS" | sed 's!"!!g')
+	echo "$(echo "$winid" | awk "NR==$i") $classid "
+done
+}
 id=$(wmctrl -lx | awk '{ print $1 " " $3 " " }' | grep " $class1.$class2 " | awk '{ print $1 }' | sed 's, ,,g')
+#id=$(xpropalt | grep "  $class1 $class2 " | awk '{ print $1 }' | sed 's, ,,g')
 echo "$id"
 file=~/.scratchpad/$arg1
 filecon=$(cat $file)
