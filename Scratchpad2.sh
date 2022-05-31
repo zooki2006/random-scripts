cf298a313bcec83ff4f52a0daf848782c3bc6feb
zooki <fleshshell@cock.li>
2022-05-31 12:30:41 +0100

    clean up and fix a bug

--- Scratchpad2.sh
+++ Scratchpad2.sh
@@ -2,23 +2,17 @@
 arg1="$1"
 arg2="$2"
 arg3="$3"
-class1="$arg1"
-class2="$arg2"
-xpropgrab () {
-class1=$(xdo id -n "$1")
-class2=$(xdo id -N "$2")
-xprop -root _NET_CLIENT_LIST | sed 's!,!\n!g' | sed 's!#!\n!' | grep -v "_NET" | sed 's, ,,' | tr 'a-z' 'A-Z' | sed 's#X#x0#' | grep "$class1" | grep "$class2"
-}
-echo "$id"
-id=$(xpropgrab $class1 $class2)
+class1=$(xdo id -n "$arg1")
+class2=$(xdo id -N "$arg2")
+if ! [ "$class1" = "" ]
+then
+	id=$(xprop -root _NET_CLIENT_LIST | sed 's!,!\n!g' | sed 's!#!\n!' | grep -v "_NET" | sed 's, ,,' | tr 'a-z' 'A-Z' | sed 's#X#x0#' | grep "$class1" | grep "$class2")
+fi
 file=~/.scratchpad/$arg1
 touch $file
 filecon=$(cat $file)
-#echo "t"
-#cat "$file"
-#echo "t"
 echo "$id"
-if [ -z "$id" ]  && ! [ -z "$filecon" ] 
+if [ -z "$id" ] && ! [ -z "$filecon" ] 
 then
 	id=$filecon
 fi
